b038d39af154bc7ef145b56e792c487b011386e0
Ice Guo <maosilu.ice@gmail.com>
2017-11-16 15:37:40 +0800

    接口的继承，类对接口的实现，以及类的比较

--- interface06.php
+++ interface06.php
@@ -1,6 +1,10 @@
 <?php
 /*
-interface关键字用语定义接口
+1.interface关键字用语定义接口
+2.接口中定义的所有方法必须公有
+3.类可以实现多个接口，用逗号来分隔多个接口的名称，实现多个接口时，接口中的方法不能有重名
+4.类是单继承的，接口可以多继承
+5.类要实现接口，必须使用和接口中所定义的方法完全一致的方式，包含参数的写法都要一模一样，否则会导致致命错误
 */
 interface ICanEat{
 	//接口里面的额方法不需要有方法体
@@ -44,6 +48,24 @@ function checkEat($obj){
 }
 checkEat($monkey);
 
+/*
+以下测试接口是否能够继承接口
+*/
+/*
+可以用extends让接口继承接口
+*/
+interface IcanPee extends ICanEat{
+	public function pee();
+}
+// 当类实现子接口时，父接口定义的方法也需要在这个类里面具体实现
+class Human1 implements ICanPee{
+	public function pee(){
+		echo 'I can pee'."\n";
+	}
+	public function eat($food){
+
+	}
+}
 
 
 
