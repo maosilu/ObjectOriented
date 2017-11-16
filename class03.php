37b6057d29ea3972791cfac2bc72eb526a66cafc
Ice Guo <maosilu.ice@gmail.com>
2017-11-16 10:40:54 +0800

    静态成员访问总结与示例

--- class03.php
+++ class03.php
@@ -1,6 +1,16 @@
 <?php
 date_default_timezone_set('PRC');
 
+/**
+静态成员需要注意的东西：
+1.静态属性用于保存类的公有数据
+2.静态方法里面只能访问静态属性
+3.静态成员不需要实例化对象就可以访问
+4.类的内部可以通过self或者static关键字访问自身静态成员
+5.可以通过parent关键字访问父类的静态成员
+6.可以通过类的名称在类定义外部访问静态成员
+*/
+
 class Human{
 	public $name;
 	protected $height;
@@ -28,12 +38,13 @@ class NbaPlayer extends Human{
 
 	/*
 	在类的对象共用同一个静态成员的时候，可以使用static定义成员，那么所有的对象的这个属性都是一样的，而不需要再去单独设置
+	静态成员保存的是全局数据，不需要创建对象就可以访问，因此在静态方法中不能使用$this对象去访问非静态数据
 	*/
 	public static $president = 'David Stern';
 
 	public static function changepresident($newPrsdt){
-		// 在类的定义中使用静态成员的时候，用self关键字后面跟着::操作符即可，⚠️：在访问静态成员属性的时候，::后面需要跟$符号
-		self::$president = $newPrsdt;
+		// 在类的定义中使用静态成员的时候，用self或者static关键字后面跟着::操作符即可，⚠️：在访问静态成员属性的时候，::后面需要跟$符号
+		self::$president = $newPrsdt; //static::$president = $newPrsdt; 这里static与self是等同的，可以相互替换
 		echo parent::$sValue."\n";
 	}
 
