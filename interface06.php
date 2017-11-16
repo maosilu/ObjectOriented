89c68373e9ccdf3e4517132bc44862bbd64b4aff
Ice Guo <maosilu.ice@gmail.com>
2017-11-16 15:00:12 +0800

    1.接口不能实例化 2.instanceof关键字判断某个对像是否实现了某个接口

--- interface06.php
+++ interface06.php
@@ -15,5 +15,41 @@ class Human implements ICanEat{
 	}
 }
 
-$obj = new Human();
-$obj->eat('Apple');
+class Animal implements ICanEat{
+	// 实现接口，必须实现接口中所有的方法
+	public function eat($food){
+		echo 'Animal eating '.$food."\n";
+	}
+}
+
+$man = new Human();
+$man->eat('Apple');
+
+$monkey = new Animal();
+$monkey->eat('banana');
+
+//不能实例化接口，以下实例化操作是错误的
+// $eatObj = new ICanEat();
+
+
+// 用interfaceof关键字判断某个对象是否实现了某个接口
+var_dump($man instanceof ICanEat);
+
+function checkEat($obj){
+	if($obj instanceof ICanEat){
+		$obj->eat('food');
+	}else{
+		echo "The obj can't eat.\n";
+	}
+}
+checkEat($monkey);
+
+
+
+
+
+
+
+
+
+
